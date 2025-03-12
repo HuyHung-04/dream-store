dd85e8c443c9872ca145b4588b2314d1330ce4e9
Hoàng Huy Hùng <hoanghuyhung080704@gmail.com>
2025-03-12 23:05:09 +0700

    merr huyhung

---
+++ dream-fronend/.angular/cache/19.2.1/dream-angular/vite/deps_ssr/@angular_cdk_scrolling.js
@@ -0,0 +1,45 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory
+} from "./chunk-7TUPHD3Q.js";
+import "./chunk-ABMCFBUH.js";
+import "./chunk-YBU35GFW.js";
+import "./chunk-F4MFAZJG.js";
+import "./chunk-PHGGHVQS.js";
+import "./chunk-XOB36AR6.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory
+};
