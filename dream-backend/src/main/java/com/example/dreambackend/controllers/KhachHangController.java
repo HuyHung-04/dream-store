5ed548163deaa394cb35a4cb98e214ecd6adff7d
Nguyễn Trường Sơn <nguyentruongson301099@gmail.com>
2025-03-18 11:40:41 +0700

    update

--- dream-backend/src/main/java/com/example/dreambackend/controllers/KhachHangController.java
+++ dream-backend/src/main/java/com/example/dreambackend/controllers/KhachHangController.java
@@ -43,7 +43,7 @@ public class KhachHangController {
     public List<KhachHang> searchKhachHangByName(@RequestParam("ten") String ten) {
         return khachHangService.searchKhachHangByName(ten);
     }
-    @PutMapping("/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<KhachHang> getKhachHangDetail(@PathVariable Integer id) {
         KhachHang khachHang= khachHangService.getKhachHangById(id);
         return ResponseEntity.ok(khachHang);
@@ -71,5 +71,4 @@ public class KhachHangController {
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
-
 }
--- dream-backend/src/main/java/com/example/dreambackend/repositories/HoaDonChiTietRepository.java
+++ dream-backend/src/main/java/com/example/dreambackend/repositories/HoaDonChiTietRepository.java
@@ -2,14 +2,18 @@ package com.example.dreambackend.repositories;
 
 import com.example.dreambackend.entities.HoaDon;
 import com.example.dreambackend.entities.SanPhamChiTiet;
+import com.example.dreambackend.requests.HoaDonChiTietSearchRequest;
+import com.example.dreambackend.responses.HoaDonChiTietResponse;
 import com.example.dreambackend.responses.TopSanPhamResponse;
 import com.example.dreambackend.entities.HoaDonChiTiet;
+import jakarta.persistence.EntityManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -61,6 +65,91 @@ public interface HoaDonChiTietRepository extends CrudRepository<HoaDonChiTiet, I
     Optional<HoaDonChiTiet> findByHoaDonAndSanPhamChiTiet(HoaDon hoaDon, SanPhamChiTiet sanPhamChiTiet);
 
     List<HoaDonChiTiet> findByHoaDon(HoaDon hoaDon);
+
+    default List<HoaDonChiTietResponse> search(HoaDonChiTietSearchRequest hoaDonChiTietSearchRequest, EntityManager entityManager) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        StringBuilder sb = new StringBuilder();
+        sb.append("""
+                SELECT
+                    hdct.id,
+                    hdct.id_hoa_don as idHoaDon,
+                    spct.id as idSanPhamChiTiet,
+                    spct.ma as maSanPhamChiTiet,
+                    hdct.ma as maHoaDonChiTiet,
+                    ms.ten as tenMau,
+                    sz.ten as tenSize,
+                	sp.ten as tenSanPham,
+                    hd.ma as maHoaDon,
+                    hdct.ngay_tao as ngayTao,
+                    hdct.ngay_sua as ngaySua,
+                    hdct.don_gia as gia,
+                    hdct.so_luong as soLuong,
+                    hdct.trang_thai as trangThai,
+                    km.hinh_thuc_giam as hinhThucGiam,
+                    km.gia_tri_giam as giaTriGiam,
+                    COUNT(1) OVER () AS totalRecords
+                FROM hoa_don_chi_tiet hdct
+                LEFT JOIN hoa_don hd ON hd.id = hdct.id_hoa_don
+                LEFT JOIN san_pham_chi_tiet spct ON spct.id = hdct.id_san_pham_chi_tiet
+                LEFT JOIN san_pham sp ON spct.id_san_pham = sp.id
+                LEFT JOIN mau_sac ms ON ms.id = spct.id_mau_sac
+                LEFT JOIN size  sz ON sz.id = spct.id_size
+                LEFT JOIN khuyen_mai km ON km.id = spct.id_khuyen_mai
+                WHERE 1 = 1
+                """);
+        if (hoaDonChiTietSearchRequest.getIdHoaDon() != null) {
+            sb.append(" AND hdct.id_hoa_don = :idHoaDon");
+        }
+        if (hoaDonChiTietSearchRequest.getMaHoaDon() != null && !hoaDonChiTietSearchRequest.getMaHoaDon().isEmpty()) {
+            sb.append(" AND hd.ma = :maHoaDon");
+        }
+        if (hoaDonChiTietSearchRequest.getTenSanPham() != null && !hoaDonChiTietSearchRequest.getTenSanPham().isEmpty()) {
+            sb.append(" AND UPPER(sp.ten) LIKE UPPeR(:tenSanPham)");
+        }
+        if (hoaDonChiTietSearchRequest.getDonGia() != null) {
+            sb.append(" AND hdct.don_gia = :donGia");
+        }
+        if (hoaDonChiTietSearchRequest.getNgayTaoTu() != null) {
+            sb.append(" AND CAST(hdct.ngay_tao AS DATE) >= :ngayTaoTu");
+        }
+        if (hoaDonChiTietSearchRequest.getNgayTaoDen() != null) {
+            sb.append(" AND CAST(hdct.ngay_tao AS DATE) <= :ngayTaoDen");
+        }
+
+        sb.append(" ORDER BY hdct.ngay_tao DESC, hdct.ngay_sua DESC");
+
+        jakarta.persistence.Query query = entityManager.createNativeQuery(sb.toString(), "HoaDonChiTietResponseMapping");
+
+        if (hoaDonChiTietSearchRequest.getPage() != null && hoaDonChiTietSearchRequest.getPageSize() != null) {
+            query.setFirstResult((hoaDonChiTietSearchRequest.getPage() - 1) * hoaDonChiTietSearchRequest.getPageSize());
+            query.setMaxResults(hoaDonChiTietSearchRequest.getPageSize());
+        }
+        if (hoaDonChiTietSearchRequest.getIdHoaDon() != null) {
+            query.setParameter("idHoaDon", hoaDonChiTietSearchRequest.getIdHoaDon());
+        }
+        if (hoaDonChiTietSearchRequest.getMaHoaDon() != null && !hoaDonChiTietSearchRequest.getMaHoaDon().isEmpty()) {
+            query.setParameter("maHoaDon", hoaDonChiTietSearchRequest.getMaHoaDon());
+        }
+        if (hoaDonChiTietSearchRequest.getTenSanPham() != null && !hoaDonChiTietSearchRequest.getTenSanPham().isEmpty()) {
+            query.setParameter("tenSanPham", "%" + hoaDonChiTietSearchRequest.getTenSanPham() + "%");
+        }
+        if (hoaDonChiTietSearchRequest.getDonGia() != null && hoaDonChiTietSearchRequest.getDonGia() > 0) {
+            query.setParameter("donGia", hoaDonChiTietSearchRequest.getDonGia());
+        }
+        if (hoaDonChiTietSearchRequest.getNgayTaoTu() != null) {
+            query.setParameter("ngayTaoTu", dateFormat.format(hoaDonChiTietSearchRequest.getNgayTaoTu()));
+        }
+        if (hoaDonChiTietSearchRequest.getNgayTaoDen() != null) {
+            query.setParameter("ngayTaoDen", dateFormat.format(hoaDonChiTietSearchRequest.getNgayTaoDen()));
+        }
+        List<HoaDonChiTietResponse> list = query.getResultList();
+        if (list.isEmpty()) {
+            hoaDonChiTietSearchRequest.setTotalRecords(0);
+        } else {
+            hoaDonChiTietSearchRequest.setTotalRecords(list.get(0).getTotalRecords());
+        }
+        return list;
+    }
 
     Optional<HoaDonChiTiet> findBySanPhamChiTietId(Integer idSanPhamChiTiet);
 
